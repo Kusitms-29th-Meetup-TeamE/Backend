e90c21f3631c4462ab81e200052e50ea3d00e903
nohy6630 <nohy6630@gmail.com>
2024-04-09 21:44:43 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/meetup/teame/backend/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.meetup.teame.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(c -> c.disable())
+                .authorizeHttpRequests(c -> c
+                        .anyRequest().permitAll());
+        return http.build();
+    }
+}
