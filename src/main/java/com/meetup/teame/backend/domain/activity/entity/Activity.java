f8456d074299a694968fd58d08846eb9602ee006
nohy6630 <nohy6630@gmail.com>
2024-04-30 09:34:22 +0900

    Feat: 메인페이지 관련 Entity 및 관계 설정

---
+++ src/main/java/com/meetup/teame/backend/domain/activity/entity/Activity.java
@@ -0,0 +1,47 @@
+package com.meetup.teame.backend.domain.activity.entity;
+
+import com.meetup.teame.backend.domain.like.entity.ActivityLike;
+import com.meetup.teame.backend.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@Comment("활동")
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Comment("활동 제목")
+    private String title;
+
+    @Comment("활동 장소")
+    private String location;
+
+    @Comment("활동 일시")
+    private LocalDateTime time;
+
+    @Comment("참여 인원")
+    private Long currentParticipants;
+
+    @Comment("최대 참여 인원")
+    private Long maxParticipants;
+
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private List<Personality> personalities = new ArrayList<>();
+
+    @OneToMany(mappedBy = "activity", cascade = CascadeType.ALL)
+    private List<ActivityLike> activityLikes;
+}
---
+++ src/main/java/com/meetup/teame/backend/domain/experience/entity/Experience.java
@@ -0,0 +1,29 @@
+package com.meetup.teame.backend.domain.experience.entity;
+
+import com.meetup.teame.backend.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@Comment("경험")
+public class Experience {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Comment("경험 유형")
+    @Enumerated(EnumType.STRING)
+    private ExperienceType type;
+
+    @Comment("경험 설명")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
