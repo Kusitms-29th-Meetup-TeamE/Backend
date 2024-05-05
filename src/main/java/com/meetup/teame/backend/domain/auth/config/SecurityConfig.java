6c1ba32c4b8017e571aa925429c508d54aa43741
nohy6630 <nohy6630@gmail.com>
2024-05-05 12:11:03 +0900

    Feat: config 설정 수정

--- src/main/java/com/meetup/teame/backend/domain/auth/config/SecurityConfig.java
+++ src/main/java/com/meetup/teame/backend/domain/auth/config/SecurityConfig.java
@@ -97,8 +97,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/","/healths").permitAll()
-                        .anyRequest().authenticated());
+                        .anyRequest().permitAll());
 
         //세션 설정 : STATELESS
         http
