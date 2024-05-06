25c041de6b189c75f5b1248dc09989d45b32fcf6
nohy6630 <nohy6630@gmail.com>
2024-05-07 02:55:10 +0900

    Docs: swagger 작성

--- src/main/java/com/meetup/teame/backend/domain/chatroom/controller/ChatRoomController.java
+++ src/main/java/com/meetup/teame/backend/domain/chatroom/controller/ChatRoomController.java
@@ -4,6 +4,7 @@ import com.meetup.teame.backend.domain.chatroom.dto.response.GroupChatRoomRes;
 import com.meetup.teame.backend.domain.chatroom.dto.response.ReadDirectChatRoomsRes;
 import com.meetup.teame.backend.domain.chatroom.dto.response.ReadGroupChatRoomsRes;
 import com.meetup.teame.backend.domain.chatroom.service.ChatRoomService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
+    @Operation(summary = "활동 대화방(단체 채팅방) 목록 조회", description = """
+            현재 로그인한 유저의 활동 대화방을 조회합니다.
+                        
+            아직 로그인이 없어서 임시로 고정된 더미 유저의 데이터를 전달하는 식으로 구현되어 있습니다.
+                        
+            추후 로그인 적용 시에는 jwt토큰도 같이 전달해서 요청해주셔야 합니다.
+            """)
     @GetMapping("/group")
     public ResponseEntity<ReadGroupChatRoomsRes> readGroupChatRooms() {
         return ResponseEntity
                 .ok(chatRoomService.readGroupChatRooms());
     }
 
+    @Operation(summary = "경험나누기 대화방(1:1 대화방) 목록 조회", description = """
+            현재 로그인한 유저의 경험나누기 대화방을 조회합니다.
+                        
+            아직 로그인이 없어서 임시로 고정된 더미 유저의 데이터를 전달하는 식으로 구현되어 있습니다.
+                        
+            추후 로그인 적용 시에는 jwt토큰도 같이 전달해서 요청해주셔야 합니다.
+            """)
     @GetMapping("/direct")
     public ResponseEntity<ReadDirectChatRoomsRes> readDirectChatRooms() {
         return ResponseEntity
