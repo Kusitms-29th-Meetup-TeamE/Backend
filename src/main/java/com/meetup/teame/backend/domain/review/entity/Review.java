674ebc0c4f223b9233d08ac150fc60b24329d16f
nohy6630 <nohy6630@gmail.com>
2024-05-17 20:43:05 +0900

    Feat: 자정마다 후기 자동 생성

--- src/main/java/com/meetup/teame/backend/domain/review/entity/Review.java
+++ src/main/java/com/meetup/teame/backend/domain/review/entity/Review.java
@@ -1,10 +1,14 @@
 package com.meetup.teame.backend.domain.review.entity;
 
+import com.meetup.teame.backend.domain.chatroom.entity.Appointment;
 import com.meetup.teame.backend.domain.chatroom.entity.DirectChatRoom;
 import com.meetup.teame.backend.domain.experience.entity.Experience;
+import com.meetup.teame.backend.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.Comment;
+
+import java.time.LocalDate;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,21 +23,32 @@ public class Review {
     private Long id;
 
     @Comment("내용")
-    private String description;
+    private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "mentor")
-    private Experience mentor;
+    @JoinColumn(name = "mentor_id")
+    private User mentor;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "mentee")
-    private DirectChatRoom mentee;
+    @JoinColumn(name = "mentee_id")
+    private User mentee;
 
-    public static Review of(String description, Experience mentor, DirectChatRoom mentee) {
+    @Embedded
+    private Appointment appointment;
+
+    @Comment("후기 작성 여부")
+    private Boolean isWritten;
+
+    public static Review of(User mentor, User mentee, Appointment appointment) {
         return Review.builder()
-                .description(description)
                 .mentor(mentor)
                 .mentee(mentee)
+                .appointment(appointment)
+                .isWritten(false)
                 .build();
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
