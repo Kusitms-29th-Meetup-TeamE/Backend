6655a0e2ab1574a8c0de5c49d9ad06322978957e
Jeongho <fgh2585@gmail.com>
2024-05-23 11:58:22 +0900

    Feat: 유저 온보딩 정보 조히 api

---
+++ src/main/java/com/meetup/teame/backend/domain/user/dto/response/UserOnboardingRes.java
@@ -0,0 +1,31 @@
+package com.meetup.teame.backend.domain.user.dto.response;
+
+import com.meetup.teame.backend.domain.personality.Personality;
+import com.meetup.teame.backend.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class UserOnboardingRes {
+
+    private List<String> personalities = new ArrayList<>();
+
+    public static UserOnboardingRes of(User user) {
+        List<String> personalityDescriptions = user.getPersonalities().stream()
+                .map(Personality::getDescription)
+                .collect(Collectors.toList());
+
+        return UserOnboardingRes.builder()
+                .personalities(personalityDescriptions)
+                .build();
+    }
+}
